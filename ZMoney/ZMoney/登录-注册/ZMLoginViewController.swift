d81a5ff6859c497db3fb8db707e2328166a989d3
apple <874538703@qq.com>
2018-09-11 17:12:12 +0800

    上传素材

---
+++ ZMoney/ZMoney/登录-注册/ZMLoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ZMLoginViewController.swift
+//  ZMoney
+//
+//  Created by apple on 2018/9/11.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ZMLoginViewController: UIViewController {
+
+//    @IBOutlet weak var labelStr: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        labelStr.text = "啦啦啦啦啦"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
