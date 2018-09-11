13f2767fafa5c3788e1b7484b1487861c362be9f
apple <874538703@qq.com>
2018-09-11 18:53:56 +0800

    登录页UI设置

--- ZMoney/ZMoney/登录-注册/ZMLoginViewController.swift
+++ ZMoney/ZMoney/登录-注册/ZMLoginViewController.swift
@@ -10,13 +10,71 @@ import UIKit
 
 class ZMLoginViewController: UIViewController {
 
-//    @IBOutlet weak var labelStr: UILabel!
+    
+    @IBOutlet weak var languageBtn: UIButton!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var verificationCode: UITextField!
+    
+    @IBOutlet weak var AreaCodeBtn: UIButton!
+    
+    @IBOutlet weak var VerificationCodeBtn: UIButton!
+    
+    @IBOutlet weak var longinBtn: UIButton!
+    
+    @IBOutlet weak var registeredBtn: UIButton!
+    
+    @IBOutlet weak var forgotPasswordBtn: UIButton!
+    
+    @IBOutlet weak var emailLoginBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        labelStr.text = "啦啦啦啦啦"
         
+        //设置占位文字颜色
+        phoneTextField.attributedPlaceholder = NSAttributedString.init(string: "请输入手机号码", attributes: [NSAttributedStringKey.foregroundColor : UIColor.darkGray])
+        verificationCode.attributedPlaceholder = NSAttributedString.init(string: "请输入手机验证码", attributes: [NSAttributedStringKey.foregroundColor : UIColor.darkGray])
+
+    }
+    
+    /// 切换语言
+    @IBAction func switchLanguages(_ sender: UIButton) {
+        print("切换语言")
+    }
+    
+    /// 选择区号
+    @IBAction func selectAreaCode(_ sender: UIButton) {
+        print("选择区号")
+    }
+    
+    /// 获取验证码
+    @IBAction func getVerificationCode(_ sender: UIButton) {
+        print("获取验证码")
+    }
+    
+    /// 登录
+    @IBAction func Longin(_ sender: UIButton) {
+        print("登录")
+    }
+    
+    /// 注册
+    @IBAction func registered(_ sender: UIButton) {
+        print("注册")
+    }
+    
+    /// 忘记密码
+    @IBAction func forgotPassword(_ sender: UIButton) {
+        print("忘记密码")
+    }
+    
+    /// 邮箱登录
+    @IBAction func emailLogin(_ sender: UIButton) {
+        print("邮箱登录")
+    }
+
+    /// 收起键盘
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
